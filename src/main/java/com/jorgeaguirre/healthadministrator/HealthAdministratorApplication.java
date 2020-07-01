f4c2ca094e5715d2dd7c8e8e80651f51133ac3d3
Jorge-Aguirre <jagojr.a@gmail.com>
2020-07-01 19:01:31 -0400

    Initial spring boot project file structure.

---
+++ src/main/java/com/jorgeaguirre/healthadministrator/HealthAdministratorApplication.java
@@ -0,0 +1,13 @@
+package com.jorgeaguirre.healthadministrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthAdministratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthAdministratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jorgeaguirre/healthadministrator/HealthAdministratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorgeaguirre.healthadministrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthAdministratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
