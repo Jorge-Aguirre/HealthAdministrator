bc921161a92abdd8409663a2ae13256b9e9e108d
Jorge Aguirre <jagojr.a@gmail.com>
2020-07-03 15:09:18 -0400

    Frontend:  - Added hospital service  - Added hospitals-list, hospital-details, hospital-add components
     All those with @angular/cli commands

--- src/main/webapp/src/app/app.module.ts
+++ src/main/webapp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HospitalsListComponent } from './components/hospitals-list/hospitals-list.component';
+import { AddHospitalComponent } from './component/add-hospital/add-hospital.component';
+import { HospitalDetailsComponent } from './components/hospital-details/hospital-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HospitalsListComponent,
+    AddHospitalComponent,
+    HospitalDetailsComponent
   ],
   imports: [
     BrowserModule,
